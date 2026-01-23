5860091150ec1968f72961382b27ff76a5279355
harry-kaur <harnoorkaur16165@gmail.com>
2026-01-23 23:34:54 +0530

    added personSchema

---
+++ models/person.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+
+// Define the Person schema
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+   party: {
+    type: String,
+    required: true
+  },
+   age: {
+    type:Number,
+    
+  },
+  
+ work: {
+    type:string,
+    enum:['chef','waiter'],
+    required: true
+  },
+  mobile: {
+    type:String,
+    required: true
+  },
+  email: {
+    type:String,
+    required: true,
+    unique:true,
+  },
+});
