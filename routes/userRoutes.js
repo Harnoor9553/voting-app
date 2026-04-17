f297e48a14e1442967023bbe32f9f1f57ba064bb
harry-kaur <harnoorkaur16165@gmail.com>
2026-04-17 12:23:22 +0530

    update signin sign up on userRoute.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,34 +1,65 @@
-// userRoutes.js
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
-const { jwtAuthMiddleware, generateToken } = require('../jwt');
+const User = require('../models/user');
+const { jwtAuthMiddleware, generateToken } = require('./../jwt');
 
-// POST route to add a person
+// SIGNUP
 router.post('/signup', async (req, res) => {
   try {
-    const data = req.body; // Assuming the request body contains the person data
+    const data = req.body;
 
-    // Create a new Person document using the Mongoose model
-    const newPerson = new Person(data);
-
-    // Save the new person to the database
-    const response = await newPerson.save();
-    console.log('data saved');
+    const newUser = new User(data);
+    const response = await newUser.save();
 
     const payload = {
       id: response.id,
       username: response.username
     };
 
-    console.log(JSON.stringify(payload));
     const token = generateToken(payload);
-    console.log("Token is : ", token);
 
-    res.status(200).json({ response: response, token: token });
+    res.status(200).json({ response, token });
+
   } catch (err) {
     console.log(err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+// LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const { aadharCardNumber, password } = req.body;
+
+    const user = await User.findOne({ aadharCardNumber });
+
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+
+    const payload = { id: user.id };
+    const token = generateToken(payload);
+
+    res.json({ token });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+// PROFILE (Protected)
+router.get('/profile', jwtAuthMiddleware, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+
+    res.status(200).json(user);
+
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
